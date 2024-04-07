462bc7052e5973827eb9877f7b57ac3c609026b0
Adrian <adrianl2710@wp.pl>
2024-04-07 11:42:01 +0200

    Zadanie 2 poprawione

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
+import geometry.Circle;
+import geometry.Point;
 
 public class Main {
     public static void main(String[] args) {
         Point test_point = new Point(1.0, 2.0);
         
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x=" + test_point.getX());
-        System.out.println("y=" + test_point.getY());
+        Circle circle = new Circle(test_point, 5);
+        
+        System.out.println("Właściwości koła:");
+        System.out.println("Środek koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + circle.getRadius());
+        System.out.println("Obwód koła: " + circle.calculatePerimeter());
+        System.out.println("Pole powierzchni koła: " + circle.getArea());
     }
 }
