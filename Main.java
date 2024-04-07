4b87ce666534395546226c00d3a4ff1ef1855b67
Adrian <adrianl2710@wp.pl>
2024-04-07 11:57:05 +0200

    Zadanie 5 wykonane

--- Main.java
+++ Main.java
@@ -1,24 +1,11 @@
-import geometry.Point;
-import geometry.Circle;
-import geometry.ColoredCircle;
+import geometry.Rectangle;
 
 
 public class Main {
     public static void main(String[] args) {
-        Circle[] circles = new Circle[2];
+       Rectangle rectangle = new Rectangle(2, 2);
 
-        circles[0] = new Circle(new Point(2, 4), 5);
-        circles[1] = new ColoredCircle(new Point(1, 1), 2, "red");
-
-        
-        for (Circle circle : circles) {
-            double area = circle.getArea();
-            System.out.println("Pole koła: " + area);
-
-            if (circle instanceof ColoredCircle) {
-                String color = ((ColoredCircle) circle).getColor();
-                System.out.println("Kolor koła: " + color);
-            }
-        }
+       System.out.println("Pole prostokąta: " + rectangle.calculateArea());
+       System.out.println("Obwód prostokąta: " + rectangle.calculatePerimeter());
     }
 }
