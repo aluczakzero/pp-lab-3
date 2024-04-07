0946b9e3cfeac58760837cb846f83a7ad7c549d1
Adrian <adrianl2710@wp.pl>
2024-04-07 11:06:46 +0200

    Zadanie 1 wykonane

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Point test_point = new Point(1.0, 2.0);
+        
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x=" + test_point.getX());
+        System.out.println("y=" + test_point.getY());
+    }
+}
