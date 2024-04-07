de734fff37f06ae2f07e2b5268114e1dcf2a505f
Adrian <adrianl2710@wp.pl>
2024-04-07 11:45:33 +0200

    Zadanie 3 poprawione

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
+import geometry.Point;
+import geometry.ColoredCircle;
 
 public class Main {
     public static void main(String[] args) {
-        Point test_point = new Point(1.0, 2.0);
+
+        Point test_point = new Point(0, 0);
         
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x=" + test_point.getX());
-        System.out.println("y=" + test_point.getY());
+        // Tworzenie obiektu klasy ColoredCircle i inicjowanie wartościami
+        ColoredCircle coloredCircle = new ColoredCircle(test_point, 5, "blue");
+        
+        // Wyświetlanie właściwości koła
+        System.out.println("Właściwości kolorowego koła:");
+        System.out.println("Środek kolorowego koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Promień kolorowego koła: " + coloredCircle.getRadius());
+        System.out.println("Kolor kolorowego koła: " + coloredCircle.getColor());
+        System.out.println("Obwód kolorowego koła: " + coloredCircle.calculatePerimeter());
+        System.out.println("Pole kolorowego powierzchni koła: " + coloredCircle.getArea());
     }
 }
